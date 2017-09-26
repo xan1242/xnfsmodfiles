e852a4bc169204afd5798fd5195e61d61f7ae3da
xan1242 <lplovropl@gmail.com>
2017-09-26 02:17:27 +0200

    Add postinit loading, fix some previous hacks

---
+++ XNFSModFiles/XNFSPrintTypes.h
@@ -0,0 +1,4 @@
+#pragma once
+#define PRINTTYPEINFO "INFO:"
+#define PRINTTYPEERROR "ERROR:"
+#define PRINTTYPEWARNING "WARNING:"
--- XNFSModFiles/dllmain.cpp
+++ XNFSModFiles/dllmain.cpp
@@ -1,6 +1,6 @@
 // XNFSModFile - a chunk resource file adder/replacer for BBX NFS games
 #include "..\includes\injector\injector.hpp"
-#include "..\includes\IniReader.h"
+#include "XNFSPrintTypes.h"
 
 #ifdef GAME_UG // broken
 #include "UG_addresses.h"
@@ -14,22 +14,27 @@
 
 #ifdef GAME_UG2
 #include "UG2_addresses.h"
+#define HAS_STAGE2
 #endif
 
 #ifdef GAME_MW
 #include "MW_addresses.h"
+#define HAS_STAGE2
 #endif
 
 #ifdef GAME_CARBON
 #include "Carbon_addresses.h"
+#define HAS_STAGE2
 #endif
 
 #ifdef GAME_PS
 #include "ProStreet_addresses.h"
+#define HAS_STAGE2
 #endif
 
-#ifdef GAME_UC // no idea if it even works
+#ifdef GAME_UC
 #include "Undercover_addresses.h"
+#define HAS_STAGE2
 #endif
 
 unsigned int CreateResourceFileFunction = CREATERESOURCEADDRESS;
@@ -39,112 +44,103 @@ unsigned int BeginLoadingFunction = BEGINLOADINGADDRESS;
 bool bLoadAmusementChunks = 0;
 #endif
 
-#ifndef GAME_PS
-unsigned int ModFileCaveExit = CAVEEXIT;
-#endif
-
-#ifdef GAME_UC
-unsigned int SharedStringPoolAllocateFunction = STRINGPOOLALLOCATEADDRESS;
+#ifdef GAME_UG
+unsigned int UndergroundJumpPoint = CAVEEXIT;
 #endif
 
 unsigned int NumberOfFiles = 0;
 char TempStringBuffer[15];
 char* PathBuffer;
 
-int CreateResourceFile(const char* FileName, int ResourceFileType, int something1, int something2, int something3)
-{
-	_asm
-	{
-#ifdef GAME_UGARCADE
-		push 0
-		push 0
-#endif
-		push something3
-		push something2
-		push something1
-		push ResourceFileType
-		push[FileName]
-		call CreateResourceFileFunction
-	}
-}
+void*(*CreateResourceFile)(const char* FileName, int ResourceFileType, int something1, int something2, int something3) = (void*(*)(const char*, int, int, int, int)) CREATERESOURCEADDRESS;
+unsigned int(__thiscall *BeginLoading)(void* dis, int a1, int a2) = (unsigned int(__thiscall *)(void* dis, int a1, int a2))BEGINLOADINGADDRESS;
 
 #ifdef GAME_UC
-unsigned int SharedStringPoolAllocate(const char* FileName)
-{
-	_asm
-	{
-		mov esi, eax
-		push [FileName]
-		mov dword ptr [esi+0x28], 0x2000
-		mov [esi+0x2C], edi
-		call SharedStringPoolAllocateFunction
-	}
-}
+unsigned int(*SharedStringPoolAllocate)(const char* String) = (unsigned int(*)(const char*))STRINGPOOLALLOCATEADDRESS;
 #endif
 
-void BeginLoading(void(*something1)(void *), void *something2)
+void LoadResourceFile(unsigned int ResourceFileType, const char* FileName)
 {
-	_asm
-	{
-#ifndef UNDERGROUNDMODE
-		push something1
-		push something2
-
+	unsigned int* v1 = (unsigned int*)CreateResourceFile(FileName, ResourceFileType, 0, 0, 0);
 #ifdef GAME_UC
-		mov ecx, esi
-		mov [esi+0x24], eax
-#else
-		mov ecx, eax
+	v1[10] = 0x2000;
+	v1[11] = *(int*)0x00D3BDD4;
+	v1[9] = SharedStringPoolAllocate(FileName);
 #endif
-
-#endif
-		call BeginLoadingFunction
-	}
+	BeginLoading(v1, 0, 0);
 }
 
-void LoadFiles()
+void LoadFiles(unsigned int FileStage)
 {
-	CIniReader inireader("");
-	for (unsigned int i = 1; i <= NumberOfFiles; i++)
-	{
-		sprintf(TempStringBuffer, "File%d", i);
-		PathBuffer = inireader.ReadString("Mods", TempStringBuffer, "");
-		CreateResourceFile(PathBuffer, 1, 0, 0, 0);
-
-#ifdef GAME_UC
-		SharedStringPoolAllocate(PathBuffer);
-#endif
-
-		BeginLoading(0, 0);
-	}
-
-#ifdef GAME_UGARCADE
-	if (bLoadAmusementChunks)
-		CreateResourceFile("GLOBAL\\AMUSEMENT\\GLOBALA.BUN", 1, 0, 0, 0);
+	if (!FileStage)
+		printf("%s XNFSModFiles entering stage 1 (preinit)\n", PRINTTYPEINFO);
 	else
-		CreateResourceFile("GLOBAL\\CAREER\\GLOBALA.BUN", 1, 0, 0, 0);
-#else
-	CreateResourceFile("GLOBAL\\GLOBALA.BUN", 1, 0, 0, 0);
-#endif
+		printf("%s XNFSModFiles entering stage 2 (postinit)\n", PRINTTYPEINFO);
+	FILE *fin = fopen("XNFSModFiles.txt", "r");
+	if (fin == NULL || !fin)
+	{
+		fin = fopen("scripts\\XNFSModFiles.txt", "r");
+		if (fin == NULL || !fin)
+		{
+			if (!FileStage)
+				printf("%s Couldn't open XNFSModFiles.txt\n", PRINTTYPEERROR);
+			else
+				printf("%s Couldn't open XNFSModFiles.txt at stage 2!\n", PRINTTYPEERROR);
+			return;
+		}
+	}
+	unsigned int CurrentResourceFileType = 0;
+	unsigned int CurrentFileStage = 0;
 
-#ifdef GAME_UC
-	SharedStringPoolAllocate("GLOBAL\\GLOBALA.BUN");
-#endif
+	char CurrentLine[1028];
+	char Part2[1024];
+	char CurrentFileName[1024];
 
-	BeginLoading(0, 0);
+	while (!feof(fin))
+	{
+		CurrentResourceFileType = 0;
+		if (fgets(CurrentLine, 1028, fin))
+		{
+			if (CurrentLine[0] != '#')
+			{
+				sscanf(CurrentLine, "%d,%d,%s", &CurrentFileStage, &CurrentResourceFileType, Part2);
+
+				// inline comment
+				if (strchr(Part2, '#') == NULL)
+					strcpy(CurrentFileName, Part2);
+				else
+					strncpy(CurrentFileName, Part2, Part2 - strchr(Part2, '#'));
+
+				if ((strcmp(CurrentFileName, "") != 0) && (CurrentFileStage == FileStage))
+				{
+					printf("%s Loading file %s resource type %d\n", PRINTTYPEINFO, CurrentFileName, CurrentResourceFileType);
+					LoadResourceFile(CurrentResourceFileType, CurrentFileName);
+				}
+			}
+		}
+	}
+	fclose(fin);
 }
 
-void __declspec(naked) ModFileCave()
+void LoadGlobalAChunksDetour()
 {
-	LoadFiles();
-	_asm
-	{
-#ifdef GAME_PS
-		retn
-#else
-		jmp ModFileCaveExit
+	LoadFiles(0);
+	LoadResourceFile(1, "GLOBAL\\GLOBALA.BUN");
+}
+
+#ifdef GAME_UG
+void __declspec(naked) UndergroundJump()
+{
+	LoadGlobalAChunksDetour();
+	_asm jmp UndergroundJumpPoint
+}
 #endif
-	}
+
+
+int LoadFilesStage2(unsigned int somethingidunno)
+{
+	LoadFiles(1);
+	return 0;
 }
 
 #ifdef GAME_UGARCADE
@@ -157,9 +153,21 @@ void __declspec(naked) MarkAmusementMode()
 
 int Init()
 {
-	CIniReader inireader("");
-	NumberOfFiles = inireader.ReadInteger("ModFiles", "NumberOfFiles", 0);
-	injector::MakeJMP(CAVEENTRY, ModFileCave, true);
+	freopen("CON", "w", stdout);
+	freopen("CON", "w", stderr);
+
+#ifdef GAME_UG
+	injector::MakeJMP(CAVEENTRY, UndergroundJump, true);
+#else
+	injector::MakeJMP(CAVEENTRY, LoadGlobalAChunksDetour, true);
+#endif
+
+#ifdef HAS_STAGE2
+	injector::MakeCALL(STAGE2CAVEENTRY, LoadFilesStage2, true);
+#ifdef GAME_UC
+	injector::WriteMemory<unsigned int>(STAGE2CAVEENTRY, (unsigned int)&LoadFilesStage2, true);
+#endif
+#endif
 
 #ifdef GAME_UGARCADE
 	injector::MakeJMP(CAVEENTRY2, MarkAmusementMode, true);
